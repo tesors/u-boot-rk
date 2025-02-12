7f361fc41dd1808c4b5000f9dfc79679e2114d1a
Ivan Boskovic <ivan.boskovic@teso.rs>
2025-02-12 16:47:04 +0100

    Removed extra env variables and changed mmc1 to mmc 0

--- include/configs/ixon_rk3528.h
+++ include/configs/ixon_rk3528.h
@@ -27,8 +27,8 @@
 	"test -n \"${BOOT_B_LEFT}\" || setexpr BOOT_B_LEFT 3;" \
 	"test -n \"${BOOT_DEV}\" || setenv BOOT_DEV \"mmc 0:3\";" \
 	"test -n \"${FRESET}\" || setenv FRESET \"0\";" \
-	"test -n \"${ROOT_A}\" || setenv ROOT_A \"/dev/mmcblk1p3\";" \
-	"test -n \"${ROOT_B}\" || setenv ROOT_B \"/dev/mmcblk1p4\";" \
+	"test -n \"${ROOT_A}\" || setenv ROOT_A \"/dev/mmcblk0p3\";" \
+	"test -n \"${ROOT_B}\" || setenv ROOT_B \"/dev/mmcblk0p4\";" \
 	"test -n \"${MMC_A}\" || setenv MMC_A \"mmc 0:3\";" \
 	"test -n \"${MMC_B}\" || setenv MMC_B \"mmc 0:4\";" \
 	"setenv silent 1;" \
@@ -61,7 +61,7 @@
 	"done;" \
 	"setenv bootcmd_pxe "";" \
 	"if test -n \"${bootpart}\"; then;" \
-	"	setenv bootargs \"console=${console} root=${bootpart} panic=10 rauc.slot=${raucslot} rootrw=/dev/mmcblk1p6 freset=$FRESET quiet\";" \
+	"	setenv bootargs \"console=${console} root=${bootpart} panic=10 rauc.slot=${raucslot} rootrw=/dev/mmcblk0p6 freset=$FRESET quiet\";" \
 	"	setenv FRESET \"0\";;" \
 	"	saveenv;" \
 	"else;" \
@@ -79,7 +79,6 @@
 
 #define PARTS_IXON \
 	"uuid_disk=${uuid_gpt_disk};" \
-	"name=idbloader,start=32KB,size=1136KB;"\
 	"name=uboot,start=1168KB,size=2528KB;"\
 	"name=dfu,start=4MB,size=64MB,uuid=${uuid_gpt_dfu};"\
 	"name=roota,start=68MB,size=2048MB,uuid=${uuid_gpt_roota};" \
@@ -93,8 +92,6 @@
 #ifndef CONFIG_SPL_BUILD
 #undef CONFIG_EXTRA_ENV_SETTINGS
 #define CONFIG_EXTRA_ENV_SETTINGS \
-	"dev_dtb=/boot/rk3528-radxa-e20c.dtb\0 root_a=/dev/mmcblk1p3\0 root_b=/dev/mmcblk1p4\0"\
-	"mmc_a=mmc 0:3\0 mmc_b=mmc 0:4\0 system_p=/dev/mmcblk1p5\0"\
 	ENV_MEM_LAYOUT_SETTINGS \
 	"partitions=" PARTS_IXON \
 	ROCKCHIP_DEVICE_SETTINGS \
